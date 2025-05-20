01fec2f13c5a0a8992b15157789c5f2e9c8a384f
thienstyle04 <thien074605@gmail.com>
2025-05-20 23:29:36 +0700

    Section 4.3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using th01.Models;
+using th01.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace th01.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private IProductRepository productRepository;
+        public HomeController(IProductRepository productRepository)
         {
-            _logger = logger;
+            this.productRepository = productRepository;
         }
-
         public IActionResult Index()
         {
-            return View();
+            return View(productRepository.GetTrendingProducts());
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
